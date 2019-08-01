7806cebf62fea95b62d5c55de88fbacf741cec3a
UlyanaLavrenova <lavrenovaulyana@gmail.com>
2019-08-01 08:20:52 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,10 +1,11 @@
 import random
+
 n = int(input("Enter the length of the list: "))
 A = []
-for i in range (n):
-    b = random.random()
+for i in range(n):
+    b = int(random.random()*100)
     A.append(b)
 print("A is", A)
-a = input("Which element do you want to delete?")
-A.pop(a-1)
+a = int(input("Which element do you want to delete?"))
+A.pop(a - 1)
 print(A)
